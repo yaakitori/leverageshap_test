4ff489268ee4a96446a32249d0c040a414f099cf
R. Teal Witter <rtealwitter@gmail.com>
2024-08-09 10:14:29 -0400

    give up on deterministic weights

--- shapleyestimators/estimators/__init__.py
+++ shapleyestimators/estimators/__init__.py
@@ -9,10 +9,8 @@ import scipy
 estimators = {
     'KernelSHAP': kernel_shap,
     'KernelSHAP Paired': kernel_shap_paired,
-    #'KernelSHAP Optimized': kernel_shap_optimized,
     'Official KernelSHAP': official_kernel_shap,    
     'LeverageSHAP': kernel_shap_leverage,
     'LeverageSHAP Paired' : kernel_shap_leverage_paired,
-    #'LeverageSHAP Optimized': kernel_shap_leverage_optimized,
     'Official Tree SHAP': official_tree_shap,
 }
