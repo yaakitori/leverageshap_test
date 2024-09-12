87b4568c3deca48ce5898c97e3566ea0adf18e9f
R. Teal Witter <rtealwitter@gmail.com>
2024-09-11 17:46:05 -0700

    rename to leverageshap

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import shapleyestimators as se
+import leverageshap as ls
 
 small_n = ['California', 'Diabetes', 'Adult', 'Synthetic']
 
@@ -21,18 +21,18 @@ if False:
 
     m = 1000
     for n in [10, 100, 1000]:
-        se.plot_probs(n, folder='images/')
-        se.plot_sampled_sizes(n, m, folder='images/')
+        ls.plot_probs(n, folder='images/')
+        ls.plot_sampled_sizes(n, m, folder='images/')
 
     for dataset in small_n + big_n:
-        se.visualize_predictions(dataset, folder='images/', exclude=['Official Tree SHAP'])
+        ls.visualize_predictions(dataset, folder='images/', exclude=['Official Tree SHAP'])
 
     num_runs = 10
     for dataset in small_n + big_n:
         print(dataset)
         for hyperparameter in ['sample_size', 'noise_std']:
             print(hyperparameter)
-            se.benchmark(num_runs, dataset, se.estimators, hyperparameter, get_hyperparameter_values(hyperparameter), silent=False)
+            ls.benchmark(num_runs, dataset, ls.estimators, hyperparameter, get_hyperparameter_values(hyperparameter), silent=False)
 
 # Plots
 
@@ -40,24 +40,24 @@ for y_name in ['shap_error', 'weighted_error']:
     # Performance by number of samples
     x_name = 'sample_size'
     constraints = {'noise': 0}
-    results = se.load_results(small_n + big_n, x_name, y_name, constraints)
-    se.plot_with_subplots(results, x_name, y_name, filename=f'images/{x_name}-{y_name}.pdf', log_x=True, log_y=y_name == 'shap_error')
+    results = ls.load_results(small_n + big_n, x_name, y_name, constraints)
+    ls.plot_with_subplots(results, x_name, y_name, filename=f'images/{x_name}-{y_name}.pdf', log_x=True, log_y=y_name == 'shap_error')
 
     # Performance by noise level
     x_name = 'noise'
     constraints = {'sample_size': 1000}
-    results = se.load_results(small_n + big_n, x_name, y_name, constraints)
-    se.plot_with_subplots(results, x_name, y_name, filename=f'images/{x_name}-{y_name}.pdf', log_x=True, log_y=y_name == 'shap_error')
+    results = ls.load_results(small_n + big_n, x_name, y_name, constraints)
+    ls.plot_with_subplots(results, x_name, y_name, filename=f'images/{x_name}-{y_name}.pdf', log_x=True, log_y=y_name == 'shap_error')
 
     # Performance by gamma
     x_name = 'gamma'
     constraints = {'sample_size': 1000, 'noise': 0}
-    results = se.load_results(small_n + big_n, x_name, y_name, constraints)
-    se.plot_with_subplots(results, x_name, y_name, filename=f'images/{x_name}-{y_name}.pdf', log_x=False, log_y=y_name == 'shap_error')
+    results = ls.load_results(small_n + big_n, x_name, y_name, constraints)
+    ls.plot_with_subplots(results, x_name, y_name, filename=f'images/{x_name}-{y_name}.pdf', log_x=False, log_y=y_name == 'shap_error')
 
 # Tables
 for y_name in ['shap_error', 'weighted_error']:
-    results = se.load_results(small_n + big_n, 'sample_size', y_name, {'noise': 0, 'sample_size' : 2000})
-    se.one_big_table(results, f'tables/{y_name}.tex')
+    results = ls.load_results(small_n + big_n, 'sample_size', y_name, {'noise': 0, 'sample_size' : 2000})
+    ls.one_big_table(results, f'tables/{y_name}.tex')
     #for dataset in results:
-    #    se.benchmark_table(results[dataset], f'tables/{dataset}-{y_name}.tex', print_md=False)
+    #    ls.benchmark_table(results[dataset], f'tables/{dataset}-{y_name}.tex', print_md=False)
