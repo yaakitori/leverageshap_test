daeba4a8a435306844b0961b0e58ab7fabb72eb7
R. Teal Witter <rtealwitter@gmail.com>
2025-03-18 17:40:06 -0400

    roll back gamma

--- run-gamma.py
+++ run-gamma.py
@@ -4,22 +4,19 @@ ablation_estimators = ['Kernel SHAP', 'Optimized Kernel SHAP', 'Leverage SHAP', 
 
 main_estimators = ['Kernel SHAP', 'Optimized Kernel SHAP', 'Leverage SHAP']
 
-include_estimators = ['Leverage SHAP', 'Leverage SHAP wo Paired']
-
-ns = [8, 10, 12, 14]
+ns = [10, 12, 14, 16]
+sample_size = 500
 num_runs = 100
 if True:
     for n in ns:
-        ls.benchmark_gamma(num_runs, n, include_estimators, sample_size=10*n, silent=False)
+        ls.benchmark_gamma(num_runs, n, ablation_estimators, sample_size=sample_size, silent=False)
 
 # Plots
 
 for y_name in ['shap_error', 'weighted_error']:
     # Performance by gamma
-    x_name = 'alpha'
-    constraints = {'noise': 0}
+    x_name = 'gamma'
+    constraints = {'sample_size': sample_size, 'noise': 0}
     results = ls.load_results([f'Synthetic_{n}' for n in ns], x_name, y_name, constraints, is_actual_sample_size=True)
-
-    ls.plot_with_subplots(results, x_name, y_name, filename=f'images/main_gamma_{y_name}.pdf', log_x=False, log_y=True, include_estimators=include_estimators, plot_mean=False)
-
-    #ls.plot_with_subplots(results, x_name, y_name, filename=f'images/ablation_gamma_{y_name}.pdf', log_x=True, log_y=False, include_estimators=ablation_estimators, plot_mean=True)
+    ls.plot_with_subplots(results, x_name, y_name, filename=f'images/main_gamma_{y_name}.pdf', log_x=True, log_y=False, include_estimators=main_estimators, plot_mean=False)
+    ls.plot_with_subplots(results, x_name, y_name, filename=f'images/ablation_gamma_{y_name}.pdf', log_x=True, log_y=False, include_estimators=ablation_estimators, plot_mean=True)
