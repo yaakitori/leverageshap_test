379fe7a70cc65facff64d99d8a093719bcce18e4
R. Teal Witter <rtealwitter@gmail.com>
2025-03-20 13:39:17 -0400

    change scale

--- run-gamma.py
+++ run-gamma.py
@@ -7,7 +7,7 @@ main_estimators = ['Kernel SHAP', 'Optimized Kernel SHAP', 'Leverage SHAP']
 ns = [10, 12, 14, 16]
 sample_size = 500
 num_runs = 100
-if True:
+if False:
     for n in ns:
         ls.benchmark_gamma(num_runs, n, ablation_estimators, sample_size=sample_size, silent=False)
 
@@ -18,5 +18,5 @@ for y_name in ['shap_error', 'weighted_error']:
     x_name = 'gamma'
     constraints = {'sample_size': sample_size, 'noise': 0}
     results = ls.load_results([f'Synthetic_{n}' for n in ns], x_name, y_name, constraints, is_actual_sample_size=True)
-    ls.plot_with_subplots(results, x_name, y_name, filename=f'images/main_gamma_{y_name}.pdf', log_x=True, log_y=False, include_estimators=main_estimators, plot_mean=False)
+    ls.plot_with_subplots(results, x_name, y_name, filename=f'images/main_gamma_{y_name}.pdf', log_x=True, log_y=True, include_estimators=main_estimators, plot_mean=False)
     ls.plot_with_subplots(results, x_name, y_name, filename=f'images/ablation_gamma_{y_name}.pdf', log_x=True, log_y=False, include_estimators=ablation_estimators, plot_mean=True)
